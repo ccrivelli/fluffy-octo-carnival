b800d530295ea9933e6cc4a1b8be142976b09365
ccrivelli <ccrivelli@aleron.com.au>
2017-12-20 16:30:49 +1100

    add securitycenter repositories dump and start with netaddr

--- hydra/code/main.py
+++ hydra/code/main.py
@@ -1,5 +1,7 @@
 from securitycenter import SecurityCenter5
 import  getpass
+from netaddr import *
+
 
 class SecurityCenterTool():
     
@@ -24,19 +26,56 @@ class SecurityCenterTool():
         sc.login(username, password)
 
 
-    def getRepos(self):
+    def getRepositoriesDump(self):
+
         print("Repository list:")
         response = sc.get('repository?type=All')
         print(response.json())
         for repo in response.json()['response']:
-            print(repo)
-#            repo_id_url = 'repository/'+repo['id']
+#            print(repo)
 #            print(repo['id'])
-#            response2 = sc.get(repo_id_url)
+            response2 = sc.get('repository/'+repo['id'])
+            json_response2 = response2.json()
+            print(" -- ")
+            print(json_response2)
+            repo_name = json_response2['response']['name']
+            if 'ipRange' in json_response2['response']['typeFields']:
+                repo_ip_range = response2.json()['response']['typeFields']['ipRange']
+            print("Repo: " + repo_name)
+            if 'ipRange' in json_response2['response']['typeFields']:
+                repo_ip_range = response2.json()['response']['typeFields']['ipRange']
+                print("IP Range: " + repo_ip_range)
+                print("Write to file ..")
+                filepath = "./files/repositories/" + repo_name + ".txt"
+                print(filepath)
+                f = open(filepath, 'w')
+                for subnet in repo_ip_range.split(','):
+                    f.write(subnet + '\n')
+                f.close()
+
+    def subnetListCompare(self):
+
+        subnet_list_file = "./files/master_subnet_list.txt"
+
+        # read file line by line without \n and save in a list
+        with open(subnet_list_file) as f:
+            subnet_strings = f.read().splitlines()
+
+        # define an empty list 
+        l = [] 
+        
+        # iter the string list and append to IPNetwork list
+        for s in subnet_strings:
+            subnet = IPNetwork(s)
+            l.append(subnet)	 
+
+        print("Master list .. ")
+        print(l)
 
 
 
     def logout(self):
+
         sc.logout()  
 
 
@@ -44,5 +83,14 @@ if __name__ == '__main__':
 
     sct = SecurityCenterTool()
     sct.login()
-    sct.getRepos()
+    sct.getRepositoriesDump()
+    sct.subnetListCompare()
     sct.logout()
+
+
+
+
+
+
+
+

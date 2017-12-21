d8161cb35eb5c8796fa70cff32d0d6cd1373dead
ccrivelli <ccrivelli@aleron.com.au>
2017-12-21 15:57:59 +1100

    new functions and trying to match and split the IP Ranges inside the file

--- hydra/code/main.py
+++ hydra/code/main.py
@@ -1,47 +1,56 @@
 from securitycenter import SecurityCenter5
 import  getpass
 from netaddr import *
-
+from os import listdir
+import re
 
 class SecurityCenterTool():
     
     def __init__(self):
+        pass
+        # credentials
+        #global hostname
+        #hostname = input("SecurityCenter Hostname (without 'https://'): ")
+        #global username
+        #username = input("Username: ")
+
+        # hide password when typed
+        #global password
+        #password = getpass.getpass("Password: ")
+
+
+    def login(self):
 
         # credentials
         global hostname
         hostname = input("SecurityCenter Hostname (without 'https://'): ")
         global username
         username = input("Username: ")
-
+        
         # hide password when typed
         global password
         password = getpass.getpass("Password: ")
-
-
-    def login(self):
 
         print("Login to ", hostname)
         global sc
         sc = SecurityCenter5(hostname)
         sc.login(username, password)
+        print(" ")
 
+    def exportRepositories(self):
 
-    def getRepositoriesDump(self):
-
-        print("Repository list:")
+        print("++ Export Repositories into files ++")
         response = sc.get('repository?type=All')
-        print(response.json())
+        #print(response.json())
         for repo in response.json()['response']:
-#            print(repo)
-#            print(repo['id'])
+            #print(repo)
+            #print(repo['id'])
             response2 = sc.get('repository/'+repo['id'])
             json_response2 = response2.json()
-            print(" -- ")
-            print(json_response2)
+            #print(" -- ")
+            #print(json_response2)
             repo_name = json_response2['response']['name']
-            if 'ipRange' in json_response2['response']['typeFields']:
-                repo_ip_range = response2.json()['response']['typeFields']['ipRange']
-            print("Repo: " + repo_name)
+            print("Name: " + repo_name)
             if 'ipRange' in json_response2['response']['typeFields']:
                 repo_ip_range = response2.json()['response']['typeFields']['ipRange']
                 print("IP Range: " + repo_ip_range)
@@ -52,25 +61,41 @@ class SecurityCenterTool():
                 for subnet in repo_ip_range.split(','):
                     f.write(subnet + '\n')
                 f.close()
+            print(" ")
+        print(" ")
 
-    def subnetListCompare(self):
 
-        subnet_list_file = "./files/master_subnet_list.txt"
+    def fileToIPNetworkList(self, file):
+
+        ''' 
+        import a txt containing a list of subnets into a list of IPNetwork
+        '''
+
+        #subnet_list_file = "./files/master_subnet_list.txt"
 
         # read file line by line without \n and save in a list
-        with open(subnet_list_file) as f:
+        with open(file) as f:
             subnet_strings = f.read().splitlines()
+            print(subnet_strings)
+
+        # search for IP Ranges like 10.1.2.0-10.1.5.255
+        for iprange in subnet_strings:
+            if re.search('(?<=-)\w+(?)',iprange):
+                print("Found!! >> " + iprange)
+            print("iprange: " + iprange)
 
         # define an empty list 
         l = [] 
-        
+
+        ''' to be uncommented ----------------------------------        
         # iter the string list and append to IPNetwork list
         for s in subnet_strings:
             subnet = IPNetwork(s)
             l.append(subnet)	 
-
-        print("Master list .. ")
-        print(l)
+        '''
+        print("++ Convert " + file + " into IPNetwork list ++")
+        print(" ")
+        return l  
 
 
 
@@ -79,13 +104,84 @@ class SecurityCenterTool():
         sc.logout()  
 
 
+    def printMenu(self):
+   
+        print(" _________________________________________________________________ ") 
+        print(" _______ _______ _______ _     _  ______ _____ _______ __   __     ")
+        print(" |______ |______ |       |     | |_____/   |      |      \_/       ")
+        print(" ______| |______ |_____  |_____| |    \_ __|__    |       |        ")
+        print("                                                                   ")                                                           
+        print(" _______ _______ __   _ _______ _______  ______                    ")
+        print(" |       |______ | \  |    |    |______ |_____/                    ")
+        print(" |_____  |______ |  \_|    |    |______ |    \_                    ")
+        print("                                                                   ")                                                                   
+        print(" _______  _____   _____                                            ")
+        print("    |    |     | |     | |                                         ")
+        print("    |    |_____| |_____| |_____                                    ")
+        print(" _________________________________________________________________ ")                                                                          
+        print("                                                                   ") 
+        print("1. Login                       ")
+        print("2. Export repositories to file ")
+        print("3. Print master list           ")
+        print("4. Print repositories          ")
+        print("5. Compare repositories        ")  
+        print("6. Logout                      ")
+        print("                               ")
+        
+
+
+
 if __name__ == '__main__':
 
+
     sct = SecurityCenterTool()
-    sct.login()
-    sct.getRepositoriesDump()
-    sct.subnetListCompare()
-    sct.logout()
+    
+    loop=True
+    
+    while loop:
+        sct.printMenu()
+        choice = input("Enter your choice [1-6]: ")
+
+        if choice == "1":
+            sct.login()
+
+        elif choice == "2":
+            sct.exportRepositories()  
+
+        elif choice == "3":
+            subnet_list_file = "./files/master_subnet_list.txt"
+            master_list = sct.fileToIPNetworkList(subnet_list_file)
+            print(" ++ Master List ++")
+            print(master_list)
+
+        elif choice == "4":
+            print("++ List files in dir ++")
+            repo_path="./files/repositories"
+            for f in listdir(repo_path):
+                print(f)
+                repo_list = sct.fileToIPNetworkList(repo_path + "/" + f)
+                print(repo_list)
+
+
+
+
+        elif choice == "5":
+            print("++ Compare repositories (skip) ++")
+
+        elif choice == "6":
+            print("++ Logout ++")
+            try:
+                sc
+            except NameError:
+                print("Never logged in. Exit.")
+            else:
+                sct.logout()
+                print("Logged out. Exit.")
+            loop = False
+       
+        else:
+            input("Wrong option. Type any key to try again ..")
+   
 
 
 
